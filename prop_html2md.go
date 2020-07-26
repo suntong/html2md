4ad51ce3fa3ed8530266bd9228f603021354b43d
Tong Sun <suntong@cpan.org>
2020-07-26 10:47:41 -0400

    - [+] functionality implemented from goquery demo

--- prop_html2md.go
+++ prop_html2md.go
@@ -8,7 +8,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/mkideal/cli"
 )
 
@@ -19,6 +22,18 @@ func html2md(ctx *cli.Context) error {
 	ctx.JSON(ctx.RootArgv())
 	ctx.JSON(ctx.Argv())
 	fmt.Println()
+
+	url := "https://blog.golang.org/godoc-documenting-go-code"
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := doc.Find("#content")
+
+	conv := md.NewConverter(md.DomainFromURL(url), true, nil)
+	markdown := conv.Convert(content)
+
+	fmt.Println(markdown)
 
 	return nil
 }
