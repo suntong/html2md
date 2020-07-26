b61a04acfc1e11611019c5c3ac95f38f040d90f3
Tong Sun <suntong@cpan.org>
2020-07-26 11:52:41 -0400

    - [*] -d/--domain option functionality implemented

--- prop_html2md.go
+++ prop_html2md.go
@@ -8,6 +8,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
 	"github.com/PuerkitoBio/goquery"
@@ -19,17 +20,22 @@ import (
 // root handler
 
 func html2md(ctx *cli.Context) error {
-	ctx.JSON(ctx.RootArgv())
 	rootArgv = ctx.RootArgv().(*rootT)
 	// https://pkg.go.dev/github.com/mkideal/cli@v0.2.2/clis?tab=doc
 	clis.Setup(progname, rootArgv.Verbose.Value())
+	clis.Verbose(1, "%#v\n", rootArgv)
 	clis.Verbose(2, "%s\n", rootArgv.Filei.Name())
 
 	doc, err := goquery.NewDocumentFromReader(rootArgv.Filei)
 	clis.AbortOn("Reading file with goquery", err)
 	content := doc.Find(rootArgv.Sel)
 
-	conv := md.NewConverter(md.DomainFromURL(rootArgv.Filei.Name()), true, nil)
+	domain, url := rootArgv.Domain, rootArgv.Filei.Name()
+	if domain == "" && regexp.MustCompile(`(?i)^http`).MatchString(url) {
+		domain = md.DomainFromURL(url)
+	}
+	clis.Verbose(2, "domain='%s'\n", domain)
+	conv := md.NewConverter(domain, true, nil)
 	markdown := conv.Convert(content)
 
 	fmt.Println(markdown)
